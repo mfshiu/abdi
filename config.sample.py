365150432d5e983871531dea2d4404839ea20a78
你的名字 <avatar.xu@gmail.com>
2022-11-22 16:20:24 +0800

    Added

---
+++ config.sample.py
@@ -0,0 +1,17 @@
+import logging
+
+mqtt_address = "localhost"
+mqtt_port = 1883
+mqtt_keepalive = 60
+mqtt_username = "eric"
+mqtt_password = "eric123"
+
+db_user = "postgres"
+db_password = "!Qazxsw2"
+db_host = "127.0.0.1"
+db_port = "5432"
+db_database = "plant"
+
+env_noise = 0
+
+log_level = logging.DEBUG
