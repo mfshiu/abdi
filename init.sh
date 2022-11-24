eee8f3c564d7b4a924674ad910a5a2da0c2f532d
你的名字 <avatar.xu@gmail.com>
2022-11-25 00:32:14 +0800

    Arrange the project

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "source /Users/xumingfang/Work/Test/OpenDDS-3.20/setenv.sh"
-source /Users/xumingfang/Work/Test/OpenDDS-3.20/setenv.sh
-
-echo "conda activate abdi"
-conda activate abdi
-
-echo "done."
